4a214e83b29e1f3e5e5f8e2409f13b15587cc71b
helenemartin <helenemartin13@hotmail.com>
2022-02-02 16:18:38 +0000

    add border, radius and box shaddow to project item

--- src/components/slideMount/slideMount.js
+++ src/components/slideMount/slideMount.js
@@ -17,29 +17,31 @@ const Wrapper = styled.div`
 
   transition: all 300ms ease-out;
 
-  // img {
-  //   width: 100%;
-  //   position: absolute;
-  //   object-fit: contain;
-  //   z-index: 1;
-  // }
+  img {
+    width: 100%;
+    position: absolute;
+    object-fit: contain;
+    z-index: 1;
+  }
 `;
 
-const ImageWrapper = styled.div`
-  position: relative;
-`;
-const Image1 = styled.img`
-  position: relative;
-  object-fit: cover;
-  width: 100%;
-`;
+// const ImageWrapper = styled.div`
+//   position: relative;
+//   width: 400px;
+//   height: 200px;
+// `;
+// const Image1 = styled.img`
+//   position: relative;
+//   object-fit: cover;
+//   width: 100%;
+// `;
 
-const Image2 = styled.img`
-  position: absolute;
+// const Image2 = styled.img`
+//   position: absolute;
 
-  width: 100%;
-  z-index: 1;
-`;
+//   width: 100%;
+//   z-index: 1;
+// `;
 
 export default function SlideMount(props) {
   // console.log(props.diaporama);
@@ -53,16 +55,14 @@ export default function SlideMount(props) {
             to={`projects/${props.diaporama.slug}`}
             key={props.diaporama.id}
           >
-            <ImageWrapper>
-              <Image1
-                src={process.env.PUBLIC_URL + props.diaporama.slugImage}
-                alt={props.diaporama.diapo.alt}
-              />
-              <Image2
-                src={process.env.PUBLIC_URL + props.diaporama.slugSlide}
-                alt={props.diaporama.slideAlt}
-              />
-            </ImageWrapper>
+            <img
+              src={process.env.PUBLIC_URL + props.diaporama.slugImage}
+              alt={props.diaporama.diapo.alt}
+            />
+            <img
+              src={process.env.PUBLIC_URL + props.diaporama.slugSlide}
+              alt={props.diaporama.slideAlt}
+            />
           </Link>
         </CustomLink>
       </Wrapper>
