cbe6d49649c1b9cf4c6cb3df167b9d7db20c2661
Hélène Martin <helenemartin13@hotmail.com>
2022-06-22 14:59:14 +0000

    remove unsused variable for deply

--- src/components/slideMount/slideMount.js
+++ src/components/slideMount/slideMount.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import  DiaporamaData  from "../../fixtures/diaporama.json";
+
 
 import styled, { keyframes } from "styled-components";
 import { breakPoints } from "../../styling/constants";
